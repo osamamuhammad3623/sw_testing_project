21356696ac30319526e0ae98b57626ff0bb4c452
Osama Muhammad <59265090+osamamuhammad3623@users.noreply.github.com>
2023-03-13 14:31:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SW Testing Project
---
+++ sw_testing_project/src/main/java/sw_testing_project/FileManager.java
@@ -0,0 +1,10 @@
+package sw_testing_project;
+
+public class FileManager {
+
+    public static String read_file(String path){
+        String data="";
+
+        return data;
+    }
+}
