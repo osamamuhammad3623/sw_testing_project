4ba25ef3a7818cba8d633f6f20530b820c833703
Kyrillos Sawiris <kyrillos.sawiris99@gmail.com>
2023-04-07 20:17:24 +0200

    isvalid function bug fixed

--- sw_testing_project/src/main/java/sw_testing_project/CourseRecord.java
+++ sw_testing_project/src/main/java/sw_testing_project/CourseRecord.java
@@ -1,79 +1,89 @@
 
 
 public class CourseRecord {
-    public String name;
-    public String code;
-    public int full_mark;
+	public String name;
+	public String code;
+	public int full_mark;
 
-    public StudentRecord[] students; // array of students registered the course
+	public StudentRecord[] students; // array of students registered the course
 
 
-    /*
-    * A function to iterate on students & set their grades & GPAs
-    * */
-    public void process_student_records(){
+	/*
+	 * A function to iterate on students & set their grades & GPAs
+	 * */
+	public void process_student_records(){
 
-        Grade g;
-        for (StudentRecord student : students) {
+		if(this.is_valid())//check if the course data is valid 
+		{
+			Grade g;
+			for (StudentRecord student : students) {
 
-            if (student.is_valid()){
-                // calculate the student grade
-               g =   student.get_grade();
-                student.student_gpa = StudentRecord.get_gpa(g); // set the GPA based on the grade
-            }else{
-                // error handling
-            }
-        }
-    }
+				if (student.is_valid()){
+					// calculate the student grade
+					g =   student.get_grade();
+					student.student_gpa = StudentRecord.get_gpa(g); // set the GPA based on the grade
+				}else{
+					// error handling
+				}
+			}
+		}
+		else 
+		{
+			//error handling 
+		}
 
-    /*
-    * A function to validate input the course-related data
-    * */
-    public boolean is_valid(){
-        boolean result = true;
+	}
 
-        // validate course name
-        // check that first char is NOT space
-        if (Character.isSpaceChar(name.charAt(0))){
-            result = false;
-        }
-        // check that every char is alpha
-        for (int i=0; i< name.length(); i++){
-            if (!Character.isLetter(name.charAt(i))){
-                result = false;
-                break;
-            }
-        }
+	/*
+	 * A function to validate input the course-related data
+	 * */
+	public boolean is_valid(){
+		boolean result = true;
 
-        // validate course code
-        if ((code.length() == 6) || (code.length() == 7)){
-            // check that first 3 chars are alpha
-            boolean first_3_alpha = Character.isLetter(code.charAt(0))
-                    && Character.isLetter(code.charAt(1))
-                    && Character.isLetter(code.charAt(2));
+		// validate course name
+		// check that first char is NOT space
+		if (Character.isSpaceChar(name.charAt(0))){
+			result = false;
+		}
+		// check that every char is alpha
+		for (int i=0; i< name.length(); i++){
+			if (!Character.isLetter(name.charAt(i))){
+				result = false;
+				break;
+			}
+		}
 
-            // check that char 3-5 are numeric
-            boolean three_char_numeric = Character.isDigit(code.charAt(3))
-                    && Character.isDigit(code.charAt(4))
-                    && Character.isDigit(code.charAt(5));
+		// validate course code
+		if ((code.length() == 6) || (code.length() == 7)){
+			// check that first 3 chars are alpha
+			boolean first_3_alpha = Character.isLetter(code.charAt(0))
+					&& Character.isLetter(code.charAt(1))
+					&& Character.isLetter(code.charAt(2));
 
-            if (first_3_alpha && three_char_numeric){
-                // check if code is 7 chars, the 7th should be 's'
-                if (!((code.length() == 7) && (code.charAt(6) == 's'))){
-                    result = false;
-                }
-            }else{
-                result = false;
-            }
+			// check that char 3-5 are numeric
+			boolean three_char_numeric = Character.isDigit(code.charAt(3))
+					&& Character.isDigit(code.charAt(4))
+					&& Character.isDigit(code.charAt(5));
 
-        }else{
-            result = false;
-        }
+			if (first_3_alpha && three_char_numeric){
+				// check if code is 7 chars, the 7th should be 's'
+				if(code.length()==7)
+				{
+					if(code.charAt(6)!='s')
+						result = false;
+				}
+			}else{
+				result = false;
+			}
 
-        // validate course full mark
-        if(full_mark != 100){
-            result = false;
-        }
-        return result;
-    }
+		}else{
+			result = false;
+		}
+
+		// validate course full mark
+		if(full_mark != 100){
+			result = false;
+		}
+		return result;
+	}
 }
